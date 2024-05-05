d24faf6485e1b90e39c3e718ee9f1d04d9d478cd
rainerpons <rainerpons11@gmail.com>
2024-05-04 18:47:55 -0700

    Set up basic text input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [inputText, setInputText] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    console.log(`Submitted form with text '${inputText}'`);
+    event.preventDefault();
+    // TODO: Search
+  };
 
   return (
     <>
       <h1>SpankSearch</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <form onSubmit={handleSubmit}>
+          <input
+            id="searchBar"
+            type="text"
+            placeholder="Search"
+            value={inputText}
+            onChange={handleChange}
+          />
+        </form>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
